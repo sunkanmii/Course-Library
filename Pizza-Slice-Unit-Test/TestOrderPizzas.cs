7a2caca9b8d06948b5fa43b79a1d5d7a3a41e92d
Fafowora Olasunkanmi <sunkanmifafowora@gmail.com>
2020-01-20 23:56:29 +0100

    Added test class for pizza orders

---
+++ Pizza-Slice-Unit-Test/TestOrderPizzas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pizza_Slice_Unit_Test
+{
+    [TestClass]
+    public class TestOrderPizzas
+    {
+        [TestMethod]
+        public void TestPizzaOrder1()
+        {
+            string document = @"./Inputs/a_example.in";
+
+            string[] pizzaMaxNTypes = pizzaObj.MaxNType(document);
+
+            int[] pizzaOrders = pizzaObj.OrderPizza(document);
+
+            Assert.AreEqual(pizzaOrders[2], "8");
+        }
+    }
+}
